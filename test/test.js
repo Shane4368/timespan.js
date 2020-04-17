78d7c7babfb75138e7c9873f996c1185050f5424
Shane4368 <shaneharris761@yahoo.com>
2020-04-17 17:28:03 -0500

    Add tests for the remaining parse overloads

--- test/test.js
+++ test/test.js
@@ -2,6 +2,13 @@
 
 const { TimeSpan } = require("../dist/index");
 
-const timeSpan = TimeSpan.parse("2.04:10:05.006");
+// All three variables should have the same value.
+const ts1 = TimeSpan.parse("2.04:10:05.006");
+const ts2 = TimeSpan.parse("2 days 4 hrs 10 mins 5.006 secs".split(" "));
+const ts3 = TimeSpan.parse([2, 4, 10, 5, 6]);
 
-console.log(timeSpan);
+console.log(ts1);
+console.log("---------------------------------------------");
+console.log(ts2);
+console.log("---------------------------------------------");
+console.log(ts3);
